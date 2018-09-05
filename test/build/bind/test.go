0fd624bc332e2b457477ae3bd1b846f5764be4b8
ByeongSu Hong <frostornge@gmail.com>
2018-09-06 00:11:28 +0900

    Add test bind

---
+++ test/build/bind/test.go
@@ -0,0 +1,195 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package adapter
+
+import (
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+// TestABI is the input ABI used to generate the binding from.
+const TestABI = "[{\"constant\":false,\"inputs\":[],\"name\":\"a\",\"outputs\":[],\"payable\":false,\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]"
+
+// TestBin is the compiled bytecode used for deploying new contracts.
+const TestBin = `0x6080604052348015600f57600080fd5b5060858061001e6000396000f300608060405260043610603e5763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416630dbe671f81146043575b600080fd5b348015604e57600080fd5b5060556057565b005b5600a165627a7a723058204a919b0d46b3f068eec1a4f601881b4b11c616f6f93f23068dc14fafb70b10b10029`
+
+// DeployTest deploys a new Ethereum contract, binding an instance of Test to it.
+func DeployTest(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Test, error) {
+	parsed, err := abi.JSON(strings.NewReader(TestABI))
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	address, tx, contract, err := bind.DeployContract(auth, parsed, common.FromHex(TestBin), backend)
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	return address, tx, &Test{TestCaller: TestCaller{contract: contract}, TestTransactor: TestTransactor{contract: contract}, TestFilterer: TestFilterer{contract: contract}}, nil
+}
+
+// Test is an auto generated Go binding around an Ethereum contract.
+type Test struct {
+	TestCaller     // Read-only binding to the contract
+	TestTransactor // Write-only binding to the contract
+	TestFilterer   // Log filterer for contract events
+}
+
+// TestCaller is an auto generated read-only Go binding around an Ethereum contract.
+type TestCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// TestTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type TestTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// TestFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type TestFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// TestSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type TestSession struct {
+	Contract     *Test             // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// TestCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type TestCallerSession struct {
+	Contract *TestCaller   // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts // Call options to use throughout this session
+}
+
+// TestTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type TestTransactorSession struct {
+	Contract     *TestTransactor   // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// TestRaw is an auto generated low-level Go binding around an Ethereum contract.
+type TestRaw struct {
+	Contract *Test // Generic contract binding to access the raw methods on
+}
+
+// TestCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type TestCallerRaw struct {
+	Contract *TestCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// TestTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type TestTransactorRaw struct {
+	Contract *TestTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewTest creates a new instance of Test, bound to a specific deployed contract.
+func NewTest(address common.Address, backend bind.ContractBackend) (*Test, error) {
+	contract, err := bindTest(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Test{TestCaller: TestCaller{contract: contract}, TestTransactor: TestTransactor{contract: contract}, TestFilterer: TestFilterer{contract: contract}}, nil
+}
+
+// NewTestCaller creates a new read-only instance of Test, bound to a specific deployed contract.
+func NewTestCaller(address common.Address, caller bind.ContractCaller) (*TestCaller, error) {
+	contract, err := bindTest(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &TestCaller{contract: contract}, nil
+}
+
+// NewTestTransactor creates a new write-only instance of Test, bound to a specific deployed contract.
+func NewTestTransactor(address common.Address, transactor bind.ContractTransactor) (*TestTransactor, error) {
+	contract, err := bindTest(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &TestTransactor{contract: contract}, nil
+}
+
+// NewTestFilterer creates a new log filterer instance of Test, bound to a specific deployed contract.
+func NewTestFilterer(address common.Address, filterer bind.ContractFilterer) (*TestFilterer, error) {
+	contract, err := bindTest(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &TestFilterer{contract: contract}, nil
+}
+
+// bindTest binds a generic wrapper to an already deployed contract.
+func bindTest(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(TestABI))
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Test *TestRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _Test.Contract.TestCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Test *TestRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Test.Contract.TestTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Test *TestRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Test.Contract.TestTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Test *TestCallerRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+	return _Test.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Test *TestTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Test.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Test *TestTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Test.Contract.contract.Transact(opts, method, params...)
+}
+
+// A is a paid mutator transaction binding the contract method 0x0dbe671f.
+//
+// Solidity: function a() returns()
+func (_Test *TestTransactor) A(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Test.contract.Transact(opts, "a")
+}
+
+// A is a paid mutator transaction binding the contract method 0x0dbe671f.
+//
+// Solidity: function a() returns()
+func (_Test *TestSession) A() (*types.Transaction, error) {
+	return _Test.Contract.A(&_Test.TransactOpts)
+}
+
+// A is a paid mutator transaction binding the contract method 0x0dbe671f.
+//
+// Solidity: function a() returns()
+func (_Test *TestTransactorSession) A() (*types.Transaction, error) {
+	return _Test.Contract.A(&_Test.TransactOpts)
+}
